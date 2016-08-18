5593f7d22611f09fc7fce7d30509835c2ebebb01
Bill Peregoy <bill@billperegoy.com>
2016-08-18 12:35:01 -0400

    First pass at tying checkbox to controller actions.

--- app/models/segment_connector.rb
+++ app/models/segment_connector.rb
@@ -1,4 +1,14 @@
 class SegmentConnector < ActiveRecord::Base
   belongs_to :user
   belongs_to :trail_segment
+
+
+  def self.matching_id(user:, segment:)
+    segment = SegmentConnector.find_by(trail_segment_id: segment.id, user_id: user.id)
+    if segment
+      segment.id
+    else
+      nil
+    end
+  end
 end
