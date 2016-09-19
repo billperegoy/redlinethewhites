a432b786396f3f2fe88d6142e3ad3dfa1c407a3a
Bill Peregoy <bill@billperegoy.com>
2016-09-19 13:37:18 -0400

    Moved visible flags to a Model element.

--- frontend/src/Accordion.elm
+++ frontend/src/Accordion.elm
@@ -23,36 +23,39 @@ main =
 
 
 type alias Model =
-    { accordionData : List (AccordionData Msg)
+    { visible : List ( Int, Bool )
     }
+
+
+initAccordionData : List (AccordionData Msg)
+initAccordionData =
+    [ AccordionData "Accordion Item 1"
+        0
+        [ accordionSubItem [ text "Sub Item 1" ]
+        , accordionSubItem [ text "Sub Item 2" ]
+        , accordionSubItem [ text "Sub Item 3" ]
+        ]
+    , AccordionData "Accordion Item 2"
+        1
+        [ accordionSubItem [ text "Sub Item 4" ]
+        , accordionSubItem [ text "Sub Item 5" ]
+        , accordionSubItem [ text "Sub Item 6" ]
+        ]
+    , AccordionData "Accordion Item 3"
+        2
+        [ accordionSubItem [ text "Sub Item 7" ]
+        , accordionSubItem [ text "Sub Item 8" ]
+        , accordionSubItem [ text "Sub Item 9" ]
+        , accordionSubItem [ text "Sub Item 10" ]
+        , accordionSubItem [ text "Sub Item 11" ]
+        , accordionSubItem [ text "Sub Item 12" ]
+        ]
+    ]
 
 
 init : ( Model, Cmd Msg )
 init =
-    Model
-        [ AccordionData "Accordion Item 1"
-            0
-            [ accordionSubItem [ text "Sub Item 1" ]
-            , accordionSubItem [ text "Sub Item 2" ]
-            , accordionSubItem [ text "Sub Item 3" ]
-            ]
-        , AccordionData "Accordion Item 2"
-            1
-            [ accordionSubItem [ text "Sub Item 4" ]
-            , accordionSubItem [ text "Sub Item 5" ]
-            , accordionSubItem [ text "Sub Item 6" ]
-            ]
-        , AccordionData "Accordion Item 3"
-            2
-            [ accordionSubItem [ text "Sub Item 7" ]
-            , accordionSubItem [ text "Sub Item 8" ]
-            , accordionSubItem [ text "Sub Item 9" ]
-            , accordionSubItem [ text "Sub Item 10" ]
-            , accordionSubItem [ text "Sub Item 11" ]
-            , accordionSubItem [ text "Sub Item 12" ]
-            ]
-        ]
-        ! []
+    { visible = [ ( 0, False ), ( 1, False ), ( 2, False ) ] } ! []
 
 
 
@@ -65,11 +68,22 @@ type Msg
     = Toggle Int
 
 
+toggleIfSelected : Int -> ( Int, Bool ) -> ( Int, Bool )
+toggleIfSelected id tuple =
+    if id == fst tuple then
+        ( id, not (snd tuple) )
+    else
+        tuple
+
+
 update : Msg -> Model -> ( Model, Cmd a )
 update msg model =
     case msg of
         Toggle id ->
-            model ! [ toggleAccordion (itemIdString id) ]
+            { model
+                | visible = List.map (\e -> toggleIfSelected id e) model.visible
+            }
+                ! [ toggleAccordion (itemIdString id) ]
 
 
 
@@ -105,10 +119,18 @@ accordionItem data =
         ]
 
 
+accordionList : List (AccordionData Msg) -> Html Msg
+accordionList data =
+    ul [ class "accordion" ]
+        (List.map accordionItem data)
+
+
 view : Model -> Html Msg
 view model =
-    ul [ class "accordion" ]
-        (List.map accordionItem model.accordionData)
+    div []
+        [ ul [] (List.map (\e -> li [] [ text (toString (snd e)) ]) model.visible)
+        , accordionList initAccordionData
+        ]
 
 
 subscriptions : Model -> Sub Msg
