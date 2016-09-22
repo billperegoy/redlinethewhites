f764ec491754ac059b5154140ba3ee66d0129750
Bill Peregoy <bill@billperegoy.com>
2016-09-22 14:28:43 -0400

    Cleanup before JS change.

--- frontend/js/accordion.js
+++ frontend/js/accordion.js
@@ -2,7 +2,6 @@ var Accordion = (function () {
   return {
     setup: function (app) {
       app.ports.toggleAccordion.subscribe(function(id) {
-        alert(id);
         var elem = document.getElementById(id);
 
         jQuery(elem).parent().find('.submenu').slideToggle('fast');
